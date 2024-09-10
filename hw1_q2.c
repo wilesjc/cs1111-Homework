ab8c5e5c0325c0e59445409393e9c45b2da846ee
Jackson Wiles <jcwiles@colossus.it.mtu.edu>
2024-09-10 09:38:39 -0400

    commit message

--- hw1_q2.c
+++ hw1_q2.c
@@ -3,17 +3,21 @@
  *
  * @author Jackson Wiles
  * @email jcwiles@mtu.edu
- * @date 09/06/2024
+ * @date 09/05/2024
  *
  */
 #include <stdio.h>
 
 int main() {
-	double op1, op2;
+	int op1, op2;
 	char opfunction;
 
-	printf("Please enter a real number expression:");
-	scanf("%.2f %c %.2f", &op1, &opfunction, &op2);
-	printf("Operand 1: %.2f\nOperand 2: %.2f\nOperation: %c\n", op1, opfunction, op2);
+	printf("Please enter an integer expression:");
+	scanf("%d", &op1);
+	scanf( "%c", &opfunction);
+	scanf( "%d", &op2);
+	printf("Operand 1: %d\n", op1);
+	printf("Operand 2: %d\n", op2);
+	printf("Operation: %c\n", opfunction);
 }
 
