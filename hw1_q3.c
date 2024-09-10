b221213ebaaf350e18e63f2ded2a6a4a3fa233e7
Jackson Wiles <jcwiles@colossus.it.mtu.edu>
2024-09-10 13:46:43 -0400

    Finished hw1_q3

---
+++ hw1_q3.c
@@ -0,0 +1,17 @@
+/*
+ * CS1111 Week2 Homework 1
+ *
+ * @author Jackson Wiles
+ * @email jcwiles@mtu.edu
+ * @date 09/10/2024
+ *
+ */
+#include <stdio.h>
+
+int main() {
+	double rn;
+	
+	printf("Please enter a real number: ");
+	scanf("%lf", &rn);
+	printf("Exponent form: %.2e\n", rn);
+}
